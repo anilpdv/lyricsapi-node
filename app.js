53ed75c48d5e77fb56726ca552d267b1fc21126f
anilpdv <pdvanil007@gmail.com>
2019-12-08 15:25:21 +0530

    adding everythingf

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const xray = require('x-ray')
+const puppeteer = require('puppeteer')
+
 
 const x = xray()
 const app = express()
 
-app.get('/lyrics', (req, res) => {
+app.get('/lyrics', async (req, res) => {
     const url = req.query.url
-    x(url, 'div.col-xs-12.col-lg-8.text-center > div:nth-of-type(5)')((err, data) => {
-        console.log(data)
-        res.json({
-            "lyrics": data
-        })
+    const browser = await puppeteer.launch({
+        headless: true
     })
-
+    const page = await browser.newPage()
+    await page.goto(url)
+    const result = page.$eval('div.col-xs-12.col-lg-8.text-center > div:nth-of-type(5)', el => el.textContent)
+    res.send(result)
+    browser.close()
 })
 
 const port = process.env.PORT || 3000;
